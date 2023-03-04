060d4aa1aea0b596b928664a288ff103de6e17cb
Gilson-SR <gilsonsrodrigues87@gmail.com>
2023-03-03 22:55:09 -0300

    feat: implementando validação do email

--- app/backend/src/middlewares/loginValidation.ts
+++ app/backend/src/middlewares/loginValidation.ts
@@ -1,9 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
 
+const emailValidation = (email: string): boolean => {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+};
+
 export default function loginValidation(req: Request, res: Response, next: NextFunction) {
   const { email, password } = req.body;
   if (!email || !password) {
     return res.status(400).json({ message: 'All fields must be filled' });
+  }
+
+  if (!emailValidation(email) || password.length < 6) {
+    return res.status(401).json({ message: 'Invalid email or password' });
   }
 
   return next();
